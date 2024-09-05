1f9f5761f308fa6abbd6d3feb1c038c657e0714f
Artur199324 <92278971+Artur199324@users.noreply.github.com>
2024-09-05 11:04:59 +0300

    Initial Commit

---
+++ The legendary Couple/The_legendary_CoupleApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_legendary_CoupleApp.swift
+//  The legendary Couple
+//
+//  Created by Artur on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct The_legendary_CoupleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ The legendary CoupleUITests/The_legendary_CoupleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  The_legendary_CoupleUITestsLaunchTests.swift
+//  The legendary CoupleUITests
+//
+//  Created by Artur on 05.09.2024.
+//
+
+import XCTest
+
+final class The_legendary_CoupleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
